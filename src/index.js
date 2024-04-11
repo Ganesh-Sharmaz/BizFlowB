a0918fd773872d9f911a1e8b044925df73688638
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-04-11 16:07:51 +0530

    user model complete

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import dotenv from "dotenv";
-import connectDB from "./db/index.js"
+import connectDB from "./db/index.js";
 
 import { app } from "./app.js";
 
 dotenv.config({
-    path: './.env'
-})
+    path: "./.env",
+});
 
 connectDB()
-.then(() => {
-    app.on("error", () => {
-        console.log("Error:", error);
-        throw error
-    })
+    .then(() => {
+        app.on("error", () => {
+            console.log("Error:", error);
+            throw error;
+        });
 
-
-    
-    app.listen(process.env.PORT || 8002, () => {
-        console.log(`Server is running at port : ${process.env.PORT}`);
-        
+        app.listen(process.env.PORT || 8002, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        });
     })
-})
-.catch((error) => {
-    console.log("mongoDB connection failed !! ", error);
+    .catch((error) => {
+        console.log("mongoDB connection failed !! ", error);
+    });
+
+app.get("/", (req, res) => {
+    res.send("homepage");
 });
---
+++ src/models/user.model.js
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            match: [
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            ],
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: [
+                6,
+                "password length should be atleast 6 characters long",
+            ],
+            maxLength: [
+                23,
+                "password length should be less than 23 characters",
+            ],
+        },
+        avatar: {
+            type: String,
+            required: true,
+            default: "",
+        },
+        number: {
+            type: String,
+            default: "",
+        },
+        bio: {
+            type: String,
+            maxLength: [30, "Bio length should be less than 30 characters"],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const User = mongoose.model("User", userSchema);
