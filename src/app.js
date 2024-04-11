2c42a186ba92eb70df738c9f5de1f0470a5fe148
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-04-11 17:20:00 +0530

    user registration and avatar upload

--- src/app.js
+++ src/app.js
@@ -17,4 +17,8 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter)
+
 export { app };
