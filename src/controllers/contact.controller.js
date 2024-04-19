7c7e7da50549e7ac97fae1a2a0809cfe0a4b5669
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-04-19 21:42:10 +0530

    bugs solved from feedback

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -18,8 +18,9 @@ const contactUs = asyncHandler(async (req, res) => {
       throw new ApiError("Please add subject and message");
     }
   
-    const send_to = "process.env.EMAIL_USER";
-    const sent_from = "process.env.EMAIL_USER";
+    const send_to = process.env.EMAIL_USER;
+  
+    const sent_from = process.env.EMAIL_USER;
     const reply_to = user.email;
     try {
       await sendEmail(subject, message, send_to, sent_from, reply_to);
